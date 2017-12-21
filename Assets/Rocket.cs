10589c0aa2a321360ad5b878dcc6eeb820d9da46
Robert Smith <smithr122@mc-s106531.local>
2017-12-21 23:08:44 +0000

    changed rocket force

---
+++ Assets/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+    void Update () {
+        processInput();
+	}
+
+    private void processInput()
+    {
+        if (Input.GetKey((KeyCode.A)))
+        {
+
+        }
+        else if (Input.GetKey((KeyCode.D)))
+        {
+
+        }
+
+        if (Input.GetKey((KeyCode.Space)))
+        {
+            print("Space pressed");
+            rigidBody.AddRelativeForce(new Vector3(0, 50f, 0));
+        }
+    }
+}
