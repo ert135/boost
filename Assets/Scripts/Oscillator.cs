d5c8f2400c459c930b783137f8b2cd8cf62f684b
unknown <ert135@gmail.com>
2018-05-27 12:11:46 +0100

    anged folder structure

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour {
 
     [SerializeField] Vector3 movementVector;
+    [SerializeField] float period = 2f;
 
     [Range(0,1)]
     [SerializeField]
@@ -22,6 +23,16 @@ public class Oscillator : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        if (period <= Mathf.Epsilon)
+        {
+            period = 1;
+        }
+        float cycles = Time.time / period;
+
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
         Vector3 offset = movementVector * movementFactor;
         transform.position  = startingPos + offset;
 	}
